053cc0de96a48584a1899cb0f4e3455bff892254
guoard <afsharzadeh1998@gmail.com>
2021-07-17 11:14:06 +0430

    create account app and customize abstract user of django

---
+++ accounts/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# from books.models import Book
+
+
+class User(AbstractUser):
+    # wishlist = models.ManyToManyField('Book')
+    pass
